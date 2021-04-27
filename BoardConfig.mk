d260e16c9e86e4d411cd1cb19f6b16bd58a1dcaa
Alexander Koskovich <zvnexus@outlook.com>
2021-04-27 06:57:55 -0700

    raphael: Remove virtual framebuffer.
     * It's largely for testing purposes and it consumes
       a large amount of memory. If your hardware doesn't
       have graphics then it can be useful, which isn't the
       case for shipped devices.
    
    Change-Id: I51becacd30d17e71cdeb0fbe31bbe1b94262b316
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -118,7 +118,6 @@ BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware_mnt/image
 BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237
 BOARD_KERNEL_CMDLINE += loop.max_part=16
 BOARD_KERNEL_CMDLINE += swiotlb=2048
-BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
