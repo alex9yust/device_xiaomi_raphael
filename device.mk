UtsavBalar1231/android_device_xiaomi_raphael

raphael: Enable freeform window management
Change-Id: I2de7aab9d42c33989b53052266350a45982273db

device.mk
@@ -200,6 +200,10 @@ PRODUCT_PACKAGES += \
     f2fs_io \
     check_f2fs
 
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+
 # GPS
 LOC_HIDL_VERSION = 4.0
 

alex9yust/device_xiaomi_raphael

Signed-off-by: Joey Huab <joey@evolution-x.org>

device.mk
@@ -111,6 +111,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     lineage.biometrics.fingerprint.inscreen@1.0-service.raphael
 
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.software.freeform_window_management.xml
+
 # FM
 PRODUCT_PACKAGES += \
     FM2 \